e39b7e80b27b2deb861fec28d48cd3e833855a36
Ricardo <568082704@qq.com>
2020-03-13 22:28:11 +0800

    实现拖拽

--- reactEvent/src/App.js
+++ reactEvent/src/App.js
@@ -7,18 +7,47 @@ class App extends Component{
   constructor(){
     super()
     this.state={
-
+      isActive:false,
+      left:0,
+      right:0
     }
   }
-  click(a,b,c,d){
-    console.log(a,b,c,d)
+  click(val){
+    alert(val)
   }
- 
+  mouseOver(){
+    this.setState({isActive:true})
+  }
+  mouseOut(){
+    this.setState({isActive:false})
+  }
+  mouseMove(val,e){
+    // console.log(e.pageX,e.pageY,val)
+  }
+  touchStart(e){
+    console.log(e.touches[0].pageX,e.touches[0].pageY)
+  }
+  touchMove(e){
+    // console.log(e.touches[0].pageX,e.touches[0].pageY)
+    this.setState({
+      left:e.touches[0].pageX-50,
+      right:e.touches[0].pageY-50,
+    })
+  }
   render(){
     return (
       <div className="App">
-        <button type="button" onClick={(e)=>this.click(1,2,4,e)}>点击事件</button>
-        {/* <button type="button" onClick={this.click}>点击事件2</button> */}
+        <button type="button" onClick={()=>this.click("点击事件2")}>点击事件</button>
+        <div 
+          className={this.state.isActive?"box active":"box"} 
+          onMouseOver={()=>this.mouseOver()}
+          onMouseOut={()=>this.mouseOut()}
+          onMouseMove={(e)=>this.mouseMove("move",e)}
+          onTouchStart={(e)=>this.touchStart(e)}
+          onTouchMove={(e)=>this.touchMove(e)}
+          style={{left:`${this.state.left}px`,top:`${this.state.right}px`}}
+        >
+        </div>
       </div>
     )
   }
