85f263521b59bd76436a1b4fd5247c018dc2e992
Ricardo <568082704@qq.com>
2020-03-14 10:12:12 +0800

    save

--- reactEvent/src/App.js
+++ reactEvent/src/App.js
@@ -9,7 +9,8 @@ class App extends Component{
     this.state={
       isActive:false,
       left:0,
-      right:0
+      right:150,
+      amount:1
     }
     this.startX = 0
     this.startY = 0
@@ -27,6 +28,7 @@ class App extends Component{
     // console.log(e.pageX,e.pageY,val)
   }
   touchStart(e){
+    this.setState({isActive:false})
     // console.log(e.touches[0].pageX,e.touches[0].pageY)
     this.startX = e.touches[0].pageX-this.state.left
     this.startY = e.touches[0].pageY-this.state.right
@@ -39,6 +41,17 @@ class App extends Component{
       right:e.touches[0].pageY-this.startY,
     })
   }
+  touchEnd(){
+    this.setState({isActive:true})
+  }
+  changeAmount(e){
+    console.log(e.target.value)
+    let amount = e.target.value.replace(/[^\d]/g,'')
+    if (amount==='' || amount==='0') {
+      amount=1
+    }
+    this.setState({amount})
+  }
   render(){
     return (
       <div className="App">
@@ -50,8 +63,16 @@ class App extends Component{
           onMouseMove={(e)=>this.mouseMove("move",e)}
           onTouchStart={(e)=>this.touchStart(e)}
           onTouchMove={(e)=>this.touchMove(e)}
+          onTouchEnd={(e)=>this.touchEnd()}
           style={{left:`${this.state.left}px`,top:`${this.state.right}px`}}
         >
+        </div>
+        <div>
+          <input 
+            type="text" value={this.state.amount} 
+            onChange={(e)=>this.changeAmount(e)}
+          />
+          <div>数量值{this.state.amount}</div>
         </div>
       </div>
     )
---
+++ reactEvent2/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import "./assets/css/app.css"
+
+
+/*eslint-disable */
+class App extends Component{
+  constructor(){
+    super()
+    this.state={
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+      </div>
+    )
+  }
+}
+
+
+
+export default App
